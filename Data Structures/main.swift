b7d7312b74e92a29f98e0cef6e6f8f45cf594aeb
Amr Gharseldin <agharse@us.ibm.com>
2018-07-20 14:39:22 -0500

    Initial Commit

---
+++ Data Structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Data Structures
+//
+//  Created by Amr Gharseldin on 7/20/18.
+//  Copyright © 2018 Amr Gharseldin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
